367069338d3f6b3124b1a7fde7eae7bfd115346a
Rey David <cuevas.vela@gmail.com>
2020-01-24 20:29:42 -0600

    added the setup of rspec

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,7 @@
 #! /usr/bin/env ruby
+require 'nokogiri'
+require 'watir'
+require 'webdrivers'
+require 'json'
+
+require_relative '../lib/currency-scraper.rb'
--- lib/currency-scraper.rb
+++ lib/currency-scraper.rb
@@ -1,3 +1,9 @@
 require 'nokogiri'
 require 'watir'
 require 'webdrivers'
+
+class Scraper
+    def initialize()
+        @browser = Watir::Browser.new
+    end
+end
